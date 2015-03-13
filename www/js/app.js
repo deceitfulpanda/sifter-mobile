8775d51f32960795f5dd5a9483d9dd2a58e0b144
Conor Flannigan <cflanni1@jhu.edu>
2015-03-12 17:19:19 -0700

    Implement basic dashboard functionality

--- www/js/app.js
+++ www/js/app.js
@@ -17,3 +17,22 @@ angular.module('sifter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('dash', {
+    url: "/dash",
+    templateUrl: "templates/dash.html"
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/dash');
+
+});
