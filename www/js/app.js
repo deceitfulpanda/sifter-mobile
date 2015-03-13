964b79103aeaafd5035b0044bd4ba18725901276
Conor Flannigan <cflanni1@jhu.edu>
2015-03-13 15:56:46 -0700

    Implement camera functionality
    Use cordova camera plugin to hook into devices' native
    camera application -- returns base64 image encoding

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('sifter', ['ionic', 'sifter.controllers'])
+angular.module('sifter', ['ionic', 'sifter.controllers', 'sifter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -31,12 +31,6 @@ angular.module('sifter', ['ionic', 'sifter.controllers'])
     url: "/dash",
     controller: 'DashCtrl',
     templateUrl: "templates/dash.html"
-  })
-
-  // waiting page for server response
-  .state('sifting', {
-    url: "/sifting",
-    templateUrl: "templates/sifting.html"
   })
 
   // results page (indicates classification response from server)
