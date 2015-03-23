b84f3e90d2aed0a7f1d0a3815959eac6be78ec6e
Conor Flannigan <cflanni1@jhu.edu>
2015-03-22 18:36:15 -0700

    Add test for Sifter API interaction

---
+++ www/test/unit/service/sifter.js
@@ -0,0 +1,27 @@
+// Test for ImgUpload interaction service
+describe('Unit: SifterAPI', function() {
+  // Load module with service
+  beforeEach(module('sifter.services'));
+
+  var service, $httpBackend;
+  beforeEach(inject(function(SifterAPI, _$httpBackend_) {
+    service = SifterAPI;
+    $httpBackend = _$httpBackend_;
+  }));
+
+  describe('uploadImage', function() {
+
+    it('should make an ajax call to /api/imgurl', function() {
+      $httpBackend.whenPOST('https://evening-castle-4681.herokuapp.com/api/imgurl').respond({
+        classification: 'compost'
+      });
+
+      var promise = service.postImgUrl('http://www.notavalidurl.net');
+
+      promise.then(function(response) {
+        expect(response.data.classification).toBe('compost');
+      });
+      $httpBackend.flush();
+    });
+  })
+});
